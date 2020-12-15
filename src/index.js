185e84e00fa04e670d3d4ea572d030a5a74fc314
Jeff Wilkey <jeff@manscaped.com>
2020-12-15 12:44:54 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 /* Script Entry */
+import { MongoClient } from 'mongodb';
+import { dbUrl } from '../config';
+
+const client = new MongoClient(dbUrl, { useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const subscriptions = client.db('production').collection('subscriptions');
+
+    subscriptions.updateMany(
+      {
+        'lineItem.sku': 'MAN-TR2-RB',
+      },
+      {
+        $set: {
+          'lineItem.sku': 'MAN-K-RBA',
+          'lineItem.variantId':
+            'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNDIzOTA5MDY1NTM2OA==',
+        },
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
